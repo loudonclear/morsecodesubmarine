ab4e048c685281291333712a2b6a1984fac04ed1
Ocean Pak <21999617+OceanPak@users.noreply.github.com>
2019-10-19 15:02:33 -0400

    Reverse Arrow Rotation Direction

--- Assets/Scripts/speedcontroller.cs
+++ Assets/Scripts/speedcontroller.cs
@@ -7,14 +7,17 @@ public class speedcontroller : MonoBehaviour
 
     [Range(0.0f, 100.0f)]
     public float speedpercent = 0.0f;
-
-    //public Vector3 Pivot;
    
+    //void Start()
+    //{
+    //    transform.rotation = new Vector3(0f, 0f, 149f);
+    //}
+
     void Update()
     {
         //transform.position += (transform.rotation*Pivot);
         
-        transform.rotation = Quaternion.AngleAxis(speedpercent * 3.6f, Vector3.forward);
+        transform.rotation = Quaternion.AngleAxis(speedpercent * -3.6f, Vector3.forward);
  
         //transform.position -= (transform.rotation*Pivot);
        
