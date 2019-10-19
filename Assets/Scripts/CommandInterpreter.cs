2fc3e9717bed2ed6bab40ea6413fa4b6a130cfe0
loudonclear <loudon_cohen@brown.edu>
2019-10-19 15:34:55 -0400

    Basic interpreter

---
+++ Assets/Scripts/CommandInterpreter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandInterpreter : MonoBehaviour
+{
+    public static Dictionary<string, string> basicCommands = new Dictionary<string, string>() {
+        {"p", "Port"},
+        {"s", "Starboard"},
+        {"a", "Accelerate"},
+        {"d", "Deccelerate"},
+        {"o", "EnginesOff"},
+        {"f", "Fire"} };
+
+    private Dictionary<string, string> commands;
+
+    public void Start()
+    {
+        commands = basicCommands;
+    }
+
+    public void InterpretCommand(string command)
+    {
+        string function;
+        if (commands.TryGetValue(command, out function)) {
+            Invoke(function, 0f);
+        }
+    }
+
+    private void Starboard()
+    {
+        Debug.Log("Moving starboard");
+    }
+
+    private void Port()
+    {
+        Debug.Log("Moving to port");
+    }
+
+    private void Accelerate()
+    {
+        Debug.Log("Accelerating");
+    }
+
+    private void Decelerate()
+    {
+        Debug.Log("Decelerating");
+    }
+
+    private void EnginesOff()
+    {
+        Debug.Log("Engines off");
+    }
+
+    private void Fire()
+    {
+        Debug.Log("Firing");
+    }
+
+
+}
