e585b930a23ec45833b51eeaf44a5e9e3347d6af
Young Jie <young_jie_cho@brown.edu>
2019-10-19 17:31:06 -0400

    october 19

---
+++ Assets/optionmenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class optionmenu : MonoBehaviour
+{
+    public Slider volumeSlider;
+    public AudioSource volumeAudio;
+    public void VolumeController()
+    {
+        volumeAudio.volume = volumeSlider.value;
+    }
+}
