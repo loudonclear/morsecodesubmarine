4edaac368a25a9b3062712a278c85eb4d390f426
Ocean Pak <21999617+OceanPak@users.noreply.github.com>
2019-10-19 14:48:32 -0400

    Speed, Temperature and Height Gauges

---
+++ Assets/Scripts/speedcontroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speedcontroller : MonoBehaviour
+{
+
+    [Range(0.0f, 100.0f)]
+    public float speedpercent = 0.0f;
+
+    //public Vector3 Pivot;
+   
+    void Update()
+    {
+        //transform.position += (transform.rotation*Pivot);
+        
+        transform.rotation = Quaternion.AngleAxis(speedpercent * 3.6f, Vector3.forward);
+ 
+        //transform.position -= (transform.rotation*Pivot);
+       
+    }
+}
