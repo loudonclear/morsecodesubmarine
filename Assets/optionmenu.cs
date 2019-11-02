4de14c5c3f155971a552dc98318ccc85df58b71e
Young Jie <young_jie_cho@brown.edu>
2019-11-02 14:51:25 -0400

    volume changing

--- Assets/optionmenu.cs
+++ Assets/optionmenu.cs
@@ -5,10 +5,17 @@ using UnityEngine.UI;
 
 public class optionmenu : MonoBehaviour
 {
+    public static float min = 0.0f;
+    public static float max = 1.0f;
     public Slider volumeSlider;
-    public AudioSource volumeAudio;
+    void Start()
+    {
+        volumeSlider.minValue = min;
+        volumeSlider.maxValue = max;
+    }
     public void VolumeController()
     {
-        volumeAudio.volume = volumeSlider.value;
+        gameManager managerScript = GameObject.FindGameObjectWithTag("audio").GetComponent<gameManager>();
+        managerScript.volume = volumeSlider.value;
     }
 }
