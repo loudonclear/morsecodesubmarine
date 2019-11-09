15eac23712057b57ada4503cf857a2f60f6ae98d
Menoikeus <datonnguyen@gmail.com>
2019-11-09 16:35:15 -0500

    Hooked up temperature gauge and added compass

---
+++ Assets/Scripts/Gauges/Compass.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour
+{
+    public void rotate(float z) {
+        this.transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, this.transform.eulerAngles.y, z);
+    }
+}
