d8556a8b1274d7620478775f7c0e28d97ce4e570
Kmilo9999 <k.diaz99@gmail.com>
2019-11-16 15:44:21 -0500

    Dialogue System and start working on tutorial 1

---
+++ Assets/Scripts/DialogSystem/DialogueManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogText;
+
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialog(Dialogue dialogue)
+    {
+        Debug.Log("Star conversation with " + dialogue.name);
+        sentences.Clear();
+        nameText.text = dialogue.name;
+
+        foreach (string sentece in dialogue.sentences)
+        {
+            sentences.Enqueue(sentece);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialog();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogText.text = sentence;
+    }
+
+    public void EndDialog()
+    {
+        Debug.Log("End of conversation");
+    }
+}
