a6e8ca2a8cb0659ea515ad4913c93cb83f4bbbbb
Kmilo9999 <k.diaz99@gmail.com>
2019-11-23 15:50:04 -0500

    Working on Tutorial 3

---
+++ Assets/Scripts/Tutorial3/MonsterTuTorialInitialDialogue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterTuTorialInitialDialogue : DialogueState
+{
+    public MonsterTuTorialInitialDialogue(Tutorial tutorial, Dialogue dialogue) : base(tutorial, dialogue, "MonsterInitDialogue")
+    {
+
+    }
+
+    public override void OnStateEnter()
+    {
+     
+    }
+
+    public override void OnStateExit()
+    {
+     
+    }
+
+    public override void Tick()
+    {
+     
+    }
+}
---
+++ Assets/Scripts/Tutorial3/Tutorial3.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public DialogueManager dialogueManager;
+    public Button continueButton;
+    public Dialogue[] tutorialDialogs;
+    private Dialogue currentDialog;
+    public int indexCurrentDialog;
+    public GameObject morseCodeMachine;
+
+    private DialogueState currentState;
+
+    void Start()
+    {
+        indexCurrentDialog = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentState != null)
+        {
+
+            currentState.Tick();
+        }
+
+    }
+
+    public void setState(DialogueState state)
+    {
+        if (currentState != null)
+        {
+            currentState.OnStateExit();
+        }
+
+        currentState = state;
+
+        if (currentState != null)
+        {
+            currentState.OnStateEnter();
+        }
+
+    }
+
+    void TaskOnClick()
+    {
+       
+       // setState(new InitialDialogue(this, currentDialog));
+    }
+}
