96f5fca5357f086cc2856ea6e985f8753f008312
Kmilo9999 <k.diaz99@gmail.com>
2019-11-23 15:03:48 -0500

    fixed dialgues text resolution and typos.

--- Assets/Scripts/Tutorial/FirstExcercise.cs
+++ Assets/Scripts/Tutorial/FirstExcercise.cs
@@ -59,18 +59,38 @@ public class FirstExcercise : DialogueState
             string text = ((MorseCodeController)tutorial.morseCodeMachine.GetComponent<MorseCodeController>()).decodedText.text;
 
             int sentence = tutorial.dialogueManager.currentSentence();
-            if (text[text.Length-1] == 'A' && !flag1 && sentence==4)
+            int index = text.Length > 0 ? text.Length - 1 : 0;
+            
+
+            if (sentence == 4)
             {
-                Button btn = tutorial.continueButton.GetComponent<Button>();
-                btn.interactable = true;
-                btn.onClick.Invoke();
-                flag1 = true;
+                if (text == "")
+                {
+                    return;
+                }
+                else {
+                    if (text[index] == 'A' && !flag1)
+                    {
+                        Button btn = tutorial.continueButton.GetComponent<Button>();
+                        btn.interactable = true;
+                        btn.onClick.Invoke();
+                        flag1 = true;
+                    }
+                }
+                
             }
             if ( sentence == 7)
             {
+                
                 Button btn = tutorial.continueButton.GetComponent<Button>();
                 btn.interactable = false;
-                if (text[text.Length - 1] == 'S' && !flag2)
+
+                if (text == "")
+                {
+                    return;
+                }
+                
+                if (text[index] == 'S' && !flag2)
                 {
                     btn.onClick.Invoke();
                     flag2 = true;
